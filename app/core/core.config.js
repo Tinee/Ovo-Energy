34b010165dacb17bd5c926eb5361e1c704c2e3bc
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-08 12:59:51 +0100

    Change to a better sutied name.

---
+++ app/core/core.config.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+
+  angular.module('app.core').config(CoreConfig);
+
+  CoreConfig.$inject = [];
+  function CoreConfig() {
+      // Config normally goes in here, but my app is way to small to need any. hehe :)
+  }
+})();
