04e8b0c52643e6767c610560c62b6498a4534531
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-08 13:00:06 +0100

    Created a navbar and a shell module.

---
+++ app/shell/navbar/navbar.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.shell')
+        .controller('NavbarController', NavbarController);
+
+    NavbarController.$inject = [];
+    function NavbarController() {
+        var vm = this;
+
+        vm.brand = '';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            vm.brand = 'Evo-Energy'
+        }
+    }
+})();
