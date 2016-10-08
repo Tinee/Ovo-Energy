a9aab3253de7e4f6d5c36186df1908691733179d
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-08 12:27:25 +0100

    Added everything for the game module.

---
+++ app/game/game.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.game')
+    .controller('GameController', GameController);
+
+  GameController.$inject = [];
+  function GameController() {
+    var vm = this;
+    activate();
+
+    function activate() {
+
+    }
+  }
+})();
