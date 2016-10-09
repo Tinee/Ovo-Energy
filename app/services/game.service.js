6798f7036abb069950a5a9a86b26712dfb29b7ff
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-09 20:15:49 +0100

    Added game logic for game.service.

--- app/services/game.service.js
+++ app/services/game.service.js
@@ -5,18 +5,39 @@
         .module('app.services')
         .factory('GameService', GameService);
 
-    GameService.$inject = [];
-    function GameService() {
+    GameService.$inject = ['CpuService'];
+    function GameService(cpuService) {
         var services = {
-            getWinner: getWinner
+            playRockPaperScissor: playRockPaperScissor
         };
 
         return services;
 
         ////////////////
-        function getWinner(players) {
 
+        function playRockPaperScissor(players) {
+            var p1 = players[0];
+            var p2 = players[1];
+            
+            return _getWinnerOfTwoPlayers(p1, p2);
+        }
 
+        function _getWinnerOfTwoPlayers(playerOne, playerTwo) {
+            var p1 = playerOne;
+            var p2 = playerTwo;
+
+            var winningPlayer = {};
+
+            if (p1.weapon.beats === p2.weapon.id) {
+                winningPlayer = p1;
+            } else if (p2.weapon.beats === p1.weapon.id) {
+                winningPlayer = p2;
+            }
+            else {
+                // Returns draw.
+                return null;
+            }
+            return winningPlayer;
         }
     }
 })();
