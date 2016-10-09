b010509bd86ef9688cb6d71ca94136d797221e64
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-09 10:45:09 +0100

    Added a location function.
    To move to play state.

--- app/game/game.js
+++ app/game/game.js
@@ -5,13 +5,19 @@
     .module('app.game')
     .controller('GameController', GameController);
 
-  GameController.$inject = [];
-  function GameController() {
+  GameController.$inject = ['$location'];
+  function GameController($location) {
     var vm = this;
+
+    vm.play = play;
+
     activate();
 
     function activate() {
+    }
 
+    function play(){
+      $location.path('play');
     }
   }
 })();
