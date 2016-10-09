f351653e073e4d9d1bba045771b0424471ac4560
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-09 10:44:14 +0100

    Created play component.

---
+++ app/game/play/play.js
@@ -0,0 +1,21 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app.game')
+        .controller('PlayController', PlayController);
+
+    PlayController.$inject = [];
+    function PlayController() {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+
+        
+    }
+})();
