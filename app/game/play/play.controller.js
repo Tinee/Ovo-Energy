35f3607699bb98cf40ff9b47896ee1013c0dd4c8
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-10-10 18:47:48 +0100

    Added all the logic for play.controller.

--- app/game/play/play.controller.js
+++ app/game/play/play.controller.js
@@ -5,10 +5,17 @@
         .module('app.game')
         .controller('PlayController', PlayController);
 
-    PlayController.$inject = ['WeaponService', 'GameService'];
-    function PlayController(weaponService, gameService) {
+    PlayController.$inject = ['WeaponService', 'GameService', 'CpuService'];
+    function PlayController(weaponService, gameService, cpuService) {
         var vm = this;
+
+
         vm.weapons = [];
+        vm.players = [];
+        vm.game = null;
+        vm.gameMessage = '';
+
+        vm.play = play;
 
         activate();
 
@@ -18,14 +25,26 @@
             vm.weapons = weaponService.get();
         }
 
-        function play(weapon) 
-        {
-            var player = {
-                cpu: false,
-                weapon: weapon
-            }
+        function play(playerWeapon) {
+            vm.players = gameService.createPlayerPool(playerWeapon);
+            vm.game = gameService.playRockPaperScissor(vm.players);
+            checkResult();
+        }
 
-            gameService.playRockPaperScissor(player);
+        function checkResult() {
+            var winner = vm.game.won;
+            var loser = vm.game.won;
+
+            if (vm.game.draw) {
+                vm.gameMessage = 'Draw!'
+            }
+            else {
+                if (winner.cpu) {
+                    vm.gameMessage = 'Bot Won with ' + winner.weapon.name;
+                } else {
+                    vm.gameMessage = 'You Won with ' + winner.weapon.name;
+                }
+            }
         }
     }
 })();
